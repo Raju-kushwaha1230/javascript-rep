9127dd748570c1a1f3b5986d12e4c709f4900efd
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2024-05-23 08:42:17 +0000

    js

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,11 @@
+/*
+there are two data types in js - primitive and non priitive and there are seven  priitive data types 
+and one non primtive data types ver  .  
+*/
+
+// aler(3+3) // we are using node.js not browser
+let num=23  //number
+let name="mighty" //string
+let sal=BigInt(23948343289)
+let 
+console.log(typeof(name))
---
+++ 01_basics/var.js
@@ -0,0 +1,10 @@
+/* in js you can declare a variable as using const , let, var as requirement and const use
+ for constant variable like you don't wanna change the value during runtime and let use for declare
+ variable base on  block of scope
+ don't use var cause issue block scope and fuctional scope
+*/
+const id=123
+let name='mighty'
+var city='hyderabad'
+
+console.table([id,name,city])
